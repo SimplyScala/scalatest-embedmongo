ec9ea6d5206a81afe87303f59d1ee0eaf1154f4d
ubourdon <bourdon.ugo@gmail.com>
2013-03-17 17:50:19 +0100

    initial commit

---
+++ project/ScalaTestEmbededMongoBuild.scala
@@ -0,0 +1,54 @@
+import sbt._
+import sbt.Keys._
+
+object ScalaTestEmbededMongoBuild extends Build {
+    lazy val root = Project(id = "simplyscala-server", base = file("."),
+        settings = Project.defaultSettings ++ Seq(
+            name := "scalatest-embedmongo",
+            organization := "com.github.simplyscala",
+            description := "API to use embeded mongoDb database for testing in ScalaTest",
+
+            version := "0.1-SNAPSHOT",
+
+            scalaVersion := "2.9.2",
+
+            crossScalaVersions := Seq("2.9.0", "2.9.1", "2.9.2"),
+
+            libraryDependencies ++= Seq(
+                "org.scalatest" %% "scalatest" % "1.8" % "test"
+            ),
+
+            publishMavenStyle := true,
+            publishArtifact in Test := false,
+            pomIncludeRepository := { _ => false },
+
+            pomExtra := (
+                <url>https://github.com/SimplyScala/scalatest-embedmongo</url>
+                    <licenses>
+                        <license>
+                            <name>GPLv3</name>
+                            <url>http://www.gnu.org/licenses/gpl-3.0.html</url>
+                            <distribution>repo</distribution>
+                        </license>
+                    </licenses>
+                    <scm>
+                        <url>git@github.com:SimplyScala/scalatest-embedmongo.git</url>
+                        <connection>scm:git:git@github.com:SimplyScala/scalatest-embedmongo.git</connection>
+                    </scm>
+                    <developers>
+                        <developer>
+                            <id>ugobourdon</id>
+                            <name>bourdon.ugo@gmail.com</name>
+                            <url>https://github.com/ubourdon</url>
+                        </developer>
+                    </developers>
+                ),
+
+            publishTo <<= version { v: String =>
+                val nexus = "https://oss.sonatype.org/"
+                if (v.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "content/repositories/snapshots")
+                else                             Some("releases" at nexus + "service/local/staging/deploy/maven2")
+            }
+        )
+    )
+}
---
+++ project/plugins.sbt
@@ -0,0 +1,11 @@
+resolvers += Resolver.url("sbt-plugin-releases", new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns)
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.7")
+
+addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.1.0")
+
+resolvers += Classpaths.typesafeResolver
+
+resolvers += "scct-github-repository" at "http://mtkopone.github.com/scct/maven-repo"
+
+addSbtPlugin("reaktor" % "sbt-scct" % "0.2-SNAPSHOT")
